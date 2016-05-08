882b5e8e81fc63dfbc46295da76ba90d551e2cce
Nicolas Feron <nicolas.feron@ulb.ac.be>
2016-05-08 12:38:47 +0200

    débuts site

---
+++ www/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Horeca</title>
+    <link rel="stylesheet" href="style/boostrap.min.css">
+    <link rel="stylesheet" href="style/custom.css">
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <?-- en-tête -->
+    <div class="row">
+        <div class="col-md-2"></div> <?-- décalage -->
+        <div class="col-md-7">
+            <img src="Images\logo.png" alt="Horeca"><br>
+        </div>
+        <div class="col-md-3">
+            <br><br>
+            <a href="signIn.php" class="btn btn-info">sign in</a>
+            <a href="signUp.php" class="btn btn-info">sign up</a>
+        </div>
+    </div>
+</nav>
+<div class="container">
